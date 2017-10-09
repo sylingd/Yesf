f3d029482099fdce49f03aafa3f6fff0b7607343
ShuangYa <896640547@qq.com>
2017-10-10 00:20:15 +0800

    Add composer demo

--- start.php
+++ start.php
@@ -1,6 +1,8 @@
 <?php
 //应用所在目录
 define("APP_PATH",  __DIR__ . '/application/');
+//使用Composer
+// define("VENDOR_DIR", __DIR__ . '/vendor/');
 require('framework/Yesf.php');
 //初始化
 $app  = new yesf\Yesf(APP_PATH . "/Config.ini");
