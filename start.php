73583f2ca9c550be6dca1fa7d21e91959db1fccf
ShuangYa <896640547@qq.com>
2018-02-25 15:38:40 +0800

    Remove some files in order to prepare for packagist

--- start.php
+++
@@ -1,9 +0,0 @@
-<?php
-//应用所在目录
-define("APP_PATH",  __DIR__ . '/application/');
-//使用Composer
-// define("VENDOR_DIR", __DIR__ . '/vendor/');
-require('framework/Yesf.php');
-//初始化
-$app  = new yesf\Yesf(APP_PATH . "/Config.ini");
-$app->bootstrap()->run();
