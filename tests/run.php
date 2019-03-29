e8933ce53bc8f0637e9373147a6f801df7d55d15
ShuangYa <896640547@qq.com>
2019-03-30 00:00:31 +0800

    Update tests

--- tests/run.php
+++ tests/run.php
@@ -1,13 +1,17 @@
 <?php
 $exit_status = 0;
 go(function () use (&$exit_status) {
-    global $argc, $argv;
-    try {
-        require __DIR__ . '/../vendor/phpunit/phpunit/phpunit';
-    } catch (\Swoole\ExitException $e) {
-        $exit_status = $e->getStatus();
-        return;
-    }
+	global $argc, $argv;
+	try {
+		require __DIR__ . '/../vendor/phpunit/phpunit/phpunit';
+	} catch (\Swoole\ExitException $e) {
+		echo $e;
+		$exit_status = $e->getStatus();
+		return;
+	} catch (\Throwable $e) {
+		echo $e;
+		$exit_status = 1;
+	}
 });
 
 \Swoole\Event::wait();
