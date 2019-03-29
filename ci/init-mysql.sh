6de50134a5a125b483ee0ae27dc674c6bef1c870
ShuangYa <896640547@qq.com>
2019-03-29 11:00:44 +0800

    Update init-mysql

--- ci/init-mysql.sh
+++ ci/init-mysql.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 main() {
-	sudo mysql -e 'CREATE DATABASE IF NOT EXISTS test;'
 	sudo mysql -e "USE mysql; UPDATE user SET password=PASSWORD('123456') where User='root'; FLUSH PRIVILEGES;"
 	sudo service mysql restart
-	mysqlimport -f -uroot -p123456 --local test $TRAVIS_BUILD_DIR/tests/TestData/mysql.sql
+	mysql -uroot -p123456 < $TRAVIS_BUILD_DIR/tests/TestData/mysql.sql
 }
 
 main
--- tests/TestData/mysql.sql
+++ tests/TestData/mysql.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `user` (
+CREATE DATABASE IF NOT EXISTS `test`;
+
+CREATE TABLE `test`.`user` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `role` tinyint(1) UNSIGNED DEFAULT NULL,
   `email` varchar(60) DEFAULT NULL,
@@ -6,12 +8,12 @@ CREATE TABLE `user` (
   `name` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `user` (`id`, `role`, `email`, `password`, `name`) VALUES
+INSERT INTO `test`.`user` (`id`, `role`, `email`, `password`, `name`) VALUES
 (1, 1, 'admin@example', '$2y$10$igsnsusA15b9VB5Vz5ukj.eNjlNOiVyunPtz9/doo0/6nF80fL4Ha', 'Administrator');
 
-ALTER TABLE `user`
+ALTER TABLE `test`.`user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
-ALTER TABLE `user`
+ALTER TABLE `test`.`user`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
