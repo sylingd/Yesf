32b26f555e4bfa25137cdc6530a504b3a7ed50f7
ShuangYa <896640547@qq.com>
2019-04-02 00:02:33 +0800

    Update ci install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,63 +1,67 @@
 #!/bin/bash
 
+installExt() {
+	stage=$(mktemp -d)
+
+	cd $stage
+	wget -O ${1}.tar.gz https://github.com/${2}/archive/${3}.tar.gz
+	tar -zxf ${1}.tar.gz
+	cd ${4}
+	phpize
+	./configure ${5}
+	make -j4
+	sudo make install
+	if [[ -f "$TRAVIS_BUILD_DIR/ci/config/${1}.ini" ]];then
+		phpenv config-add "$TRAVIS_BUILD_DIR/ci/config/${1}.ini"
+	fi
+
+	cd $TRAVIS_BUILD_DIR
+	sudo rm -rf $stage
+}
+
+installHiRedis() {
+	stage=$(mktemp -d)
+
+	cd $stage
+	wget -O hiredis.tar.gz https://github.com/redis/hiredis/archive/v${1}.tar.gz
+	tar -zxf hiredis.tar.gz
+	cd hiredis-${1}
+	make -j4
+	sudo make install
+	sudo ldconfig
+
+	cd $TRAVIS_BUILD_DIR
+	sudo rm -rf $stage
+}
+
 main() {
 	swoole_ver="4.2.13"
 	hiredis_ver="0.14.0"
 	yac_ver="2.0.2"
 	seaslog_ver="2.0.2"
-	stage=$(mktemp -d)
+	yaconf_ver="1.0.7"
 
 	# Install hiredis
-	cd $stage
-	wget -O hiredis.tar.gz https://github.com/redis/hiredis/archive/v${hiredis_ver}.tar.gz
-	tar -zxf hiredis.tar.gz
-	cd hiredis-${hiredis_ver}
-	make -j4
-	sudo make install
-	sudo ldconfig
+	installHiRedis $hiredis_ver
 
 	# Install swoole
-	cd $stage
-	wget -O swoole.tar.gz https://github.com/swoole/swoole-src/archive/v${swoole_ver}.tar.gz
-	tar -zxf swoole.tar.gz
-	cd swoole-src-${swoole_ver}
-	phpize
-	./configure --enable-sockets=yes --enable-openssl=yes --enable-mysqlnd=yes
-	make -j4
-	sudo make install
-	phpenv config-add $TRAVIS_BUILD_DIR/ci/config/swoole.ini
+	installExt "swoole" "swoole/swoole-src" "v${swoole_ver}" "swoole-src-${swoole_ver}" "--enable-sockets=yes --enable-openssl=yes --enable-mysqlnd=yes"
 
 	# Install SeasLog
 	mkdir -p /var/log/www
 	chmod -R 0777 /var/log/www
-	cd $stage
-	wget -O seaslog.tar.gz https://github.com/SeasX/SeasLog/archive/SeasLog-${seaslog_ver}.tar.gz
-	tar -zxf seaslog.tar.gz
-	cd SeasLog-SeasLog-${seaslog_ver}
-	phpize
-	./configure
-	make -j4
-	sudo make install
-	phpenv config-add $TRAVIS_BUILD_DIR/ci/config/seaslog.ini
+	installExt "seaslog" "SeasX/SeasLog" "SeasLog-${seaslog_ver}" "SeasLog-SeasLog-${seaslog_ver}"
 
 	# Install Yac
 	can_install_yac=$(php -r "echo version_compare(PHP_VERSION, '7.3');")
-	if [[ "$can_install_yac" == "-1" ]]; then
-		cd $stage
-		wget -O yac.tar.gz https://github.com/laruence/yac/archive/yac-${yac_ver}.tar.gz
-		tar -zxf yac.tar.gz
-		cd yac-yac-${yac_ver}
-		phpize
-		./configure
-		make -j4
-		sudo make install
-		phpenv config-add $TRAVIS_BUILD_DIR/ci/config/yac.ini
+	if [[ "$can_install_yac" == "-1" ]];then
+		installExt "yac" "laruence/yac" "yac-${yac_ver}" "yac-yac-${yac_ver}"
 	else
 		echo -e "Skip install Yac\n"
 	fi
 
-	cd $TRAVIS_BUILD_DIR
-	sudo rm -rf $stage
+	# Install Yaconfcd $stage
+	installExt "yaconf" "laruence/yaconf" "yaconf-${yaconf_ver}" "yaconf-yaconf-${yaconf_ver}"
 }
 
 main
