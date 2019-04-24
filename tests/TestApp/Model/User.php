d693769db0c802813b37afa11eb9b964f8cfec5f
ShuangYa <896640547@qq.com>
2019-04-24 16:18:21 +0800

    Remove ModelInterface

--- tests/TestApp/Model/User.php
+++ tests/TestApp/Model/User.php
@@ -2,9 +2,8 @@
 namespace YesfApp\Model;
 
 use Yesf\ModelAbstract;
-use Yesf\ModelInterface;
 
-class User extends ModelAbstract implements ModelInterface {
+class User extends ModelAbstract {
 	protected $_table_name = 'user';
 	protected $_primary_key = 'id';
 }
