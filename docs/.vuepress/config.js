0fc2b42903bb3519c54c596270c890c826aeb1df
ShuangYa <896640547@qq.com>
2019-05-03 23:23:39 +0800

    Update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,9 +8,11 @@ module.exports = {
       includeLevel: [1, 2, 3]
     }
   },
-  plugins: ['vuepress-plugin-baidu-tongji', {
-    hm: '44b41bc63385a6b27e692272bb1fb393'
-  }],
+  plugins: [
+    ['vuepress-plugin-baidu-tongji', {
+      hm: '44b41bc63385a6b27e692272bb1fb393'
+    }]
+  ],
   locales: {
     '/': {
       lang: 'zh-CN',
@@ -51,53 +53,7 @@ module.exports = {
             }
           ]
         }],
-        sidebar: [
-          {
-            title: '开发指南',
-            children: [
-              '/zh-CN/guide/',
-              '/zh-CN/guide/install',
-              '/zh-CN/guide/structure',
-              '/zh-CN/guide/configuration',
-            ]
-          },
-          '/zh-CN/container',
-          {
-            title: '请求处理',
-            children: [
-              '/zh-CN/process_request/',
-              '/zh-CN/process_request/static',
-              '/zh-CN/process_request/router',
-              '/zh-CN/process_request/request',
-              '/zh-CN/process_request/response'
-            ]
-          },
-          {
-            title: '插件',
-            children: [
-              '/zh-CN/plugin/',
-              '/zh-CN/plugin/on_worker_start'
-            ]
-          },
-          {
-            title: '连接池',
-            children: [
-              '/zh-CN/connection/',
-              '/zh-CN/connection/write_driver.md',
-              '/zh-CN/connection/write_adapter.md'
-            ]
-          },
-          {
-            title: '缓存',
-            children: [
-              '/zh-CN/cache/',
-              '/zh-CN/cache/redis',
-              '/zh-CN/cache/file',
-              '/zh-CN/cache/yac',
-              '/zh-CN/cache/custom',
-            ]
-          }
-        ]
+        sidebar: require('./list_zh-CN')
       },
       '/en/': {
         lang: 'en-US',
@@ -122,7 +78,7 @@ module.exports = {
             }
           ]
         }],
-        sidebar: []
+        sidebar: require('./list_en')
       }
     }
   }
